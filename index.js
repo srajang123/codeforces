56c374ca7c4bb8bc8d8afa6f60312ad1719c9b24
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-02-05 22:00:05 +0530

    Initialised Codeforces

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const request = require('request');
+const expHbs = require('express-handlebars');
+const app = express();
+
+app.engine('hbs', expHbs({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+function contests() {
+    request('https://codeforces.com/api/contest.list', (err, response, body) => {
+        return JSON.parse(body).result;
+    })
+}
+
+app.get('/', (req, res, next) => {
+    console.log('Welcome');
+    res.render('home');
+})
+app.get('/contests/:type', (req, res, next) => {
+    let type = req.params.type;
+    request('https://codeforces.com/api/contest.list', (err, response, body) => {
+        let data = JSON.parse(body).result;
+        res.render('home', { data: data, type: type, contests: true, title: 'Contest List' })
+    })
+})
+app.get('/contests/', (req, res, next) => {
+    //res.render('home', { data: 'a', contests: true, title: 'Contest List' })
+    request('https://codeforces.com/api/contest.list', (err, response, body) => {
+        let data = JSON.parse(body).result;
+        res.render('home', { data: data, contests: true, title: 'Contest List' })
+    })
+})
+
+app.listen(2000, () => { console.log('Server stared at 2000') })
