becec521d2947a861a9c90fb2c5417e3338a3a4f
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-02-07 21:47:16 +0530

    Added 404, Home Page and improved routes and styles

--- index.js
+++ index.js
@@ -17,16 +17,15 @@ function contests() {
 }
 
 app.get('/', (req, res, next) => {
-    console.log('Welcome');
-    res.render('home');
-})
+    res.render('index', { title: 'CodeForces API', home: true });
+});
 app.get('/contests/:type', (req, res, next) => {
     let type = req.params.type;
     request('https://codeforces.com/api/contest.list', (err, response, body) => {
         let data = JSON.parse(body).result;
         res.render('home', { data: data, type: type, contests: true, title: 'Contest List' })
     })
-})
+});
 app.get('/contests/', (req, res, next) => {
     //res.render('home', { data: 'a', contests: true, title: 'Contest List' })
     request('https://codeforces.com/api/contest.list', (err, response, body) => {
@@ -34,5 +33,7 @@ app.get('/contests/', (req, res, next) => {
         res.render('home', { data: data, contests: true, title: 'Contest List' })
     })
 })
-
+app.use((req, res, next) => {
+    res.render('404', { title: 'Page Not Found', nofound: true });
+})
 app.listen(2000, () => { console.log('Server stared at 2000') })
--- public/js/contests.js
+++ public/js/contests.js
@@ -4,19 +4,20 @@ document.querySelector('.open-search').addEventListener('click', (e) => {
     document.querySelector('.form').classList.add('formS');
 })
 document.querySelector('.search').addEventListener('click', (e) => {
-    let title = document.querySelector('.title').value;
-    let phase = document.querySelector('.sel').value;
-    let list = document.querySelectorAll('.list');
-    list.forEach(v => {
-        if (v.children[0].children[0].innerHTML.indexOf(title) == -1 || (phase != v.children[2].innerHTML && phase != -1))
-            v.style.display = 'none';
-        else
-            v.style.display = 'table-row';
-    });
-}) {
-    let list = document.querySelectorAll('.list');
-    list.forEach(v => {
-        const d = new Date(Number(v.children[4].innerHTML));
-        v.children[4].innerHTML = d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes();
-    });
-}
+        let title = document.querySelector('.title').value;
+        let phase = document.querySelector('.sel').value;
+        let list = document.querySelectorAll('.list');
+        list.forEach(v => {
+            if (v.children[0].children[0].innerHTML.indexOf(title) == -1 || (phase != v.children[2].innerHTML && phase != -1))
+                v.style.display = 'none';
+            else
+                v.style.display = 'table-row';
+        });
+    })
+    /*{
+        let list = document.querySelectorAll('.list');
+        list.forEach(v => {
+            const d = new Date(Number(v.children[4].innerHTML));
+            v.children[4].innerHTML = d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes();
+        });
+    }*/
